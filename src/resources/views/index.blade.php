9c93bdafbf02e48f439a540361104714b00b2c67
tsushima aki <aki1120tsushima@gmail.com>
2025-08-25 21:18:24 +0900

    commit2

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,164 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection
+
+@section('content')
+
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>Contact</h2>
+    </div>
+    <form class="form">
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="first_name" placeholder="例:山田" />
+                    <input type="text" name="last_name" placeholder="例:太郎" />
+                </div>
+                <div class="form__error">
+                    @error('first_name')
+                    {{ $message }}
+                    @enderror
+                    @error('last_name')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">性別</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--radio">
+                    <label><input type="radio" name="gender" value="male" />
+                        男性
+                    </label>
+                    <label><input type="radio" name="gender" value="female" />
+                        女性
+                    </label>
+                    <label><input type="radio" name="gender" value="other" />
+                        その他
+                    </label>
+                </div>
+                <div class="form__error">
+                    @error('gender')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="例:test@example.com" />
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="tel" name="tel1" placeholder="080" />
+                    <input type="tel" name="tel2" placeholder="1234" />
+                    <input type="tel" name="tel3" placeholder="5678" />
+                </div>
+                <div class="form__error">
+                    @error('tel1')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address" placeholder="例:東京都渋谷区千駄ヶ谷1-2-3" />
+                </div>
+                <div class="form__error">
+                    @error('address')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="building" placeholder="例:千駄ヶ谷マンション101" />
+                </div>
+                <div class="form__error">
+                    @error('building')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせの種類</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--select">
+                    <select name="category" placeholder="選択してください">
+                        <option>1.商品のお届けについて</option>
+                        <option>2.商品の交換について</option>
+                        <option>3.商品トラブル</option>
+                        <option>4.ショップへのお問い合わせ</option>
+                        <option>5.その他</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--textarea">
+                    <textarea name="content" placeholder="お問い合わせ内容をご記入ください"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">確認画面</button>
+        </div>
+    </form>
+</div>
+@endsection
